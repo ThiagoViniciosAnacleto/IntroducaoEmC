0af350ddb50e65a55b61b4c772e775c5a6eee817
ThiagoViniciosAnacleto <139421532+ThiagoViniciosAnacleto@users.noreply.github.com>
2023-07-13 00:18:49 -0300

    Variaveis
    Variaveis e Regras

---
+++ variaveis.c
@@ -0,0 +1,53 @@
+//variaveis = dados
+
+int n = 10; //adiciona um valor inteiro (sem casas decimais)
+float n2 = 9.2; //adiciona um valor real ( com casas decimais)
+char letra = 'A'//adiciona apenas UM caracter, utilizar aspas simples
+char frase[10] = "Cachorro"; //adiciona varios caracteres, utiliar aspas dupla
+double n3 = 10.2334; //adiciona um valor real ( com casas decimais ) porém com dupla precisão
+
+//------------------------------------////------------------------------------////-------------------------//
+
+printf //exibe o conteudo discriminado
+scanf //seta a variavel a partir do dado discriminado
+
+//------------------------------------////------------------------------------////-------------------------//
+
+&& 5 > 4 E 3 >= 4  [ FALSO ]
+&& 5 > 4 E 6 >= 4 [ VERDADEIRO ]    // o && significa [ E ] - PRECISA QUE OS DOIS OU MAIS LADOS SEJAM VERDADEIROS
+5 > 2 && 8 >= 6 [ VERDADEIRO ]
+
+//------------------------------------////------------------------------------////-------------------------//
+
+|| 5 > 4 OU 3 >= 4 [ VERDADEIRO ]
+|| 5 > 6 OU 3 >= 4 [ FALSO ]
+7 > 6 || 6 >= 5 [ VERDADEIRO ]    // o || significa [ OU ] - PRECISA QUE APENAS UM LADO SEJA VERDADEIRO
+8 > 3 || 5 < 6 [ VERDADEIRO]  
+
+//------------------------------------////------------------------------------////-------------------------//
+
+! 3 > 3 [ VERDADEIRO ] 
+! 3 > 2 [ VERDADEIRO ] // o ! INVERTE o valor do resultado, ou seja 3 é sim maior que dois, porém foi invertido
+! 3 < 1 [ VERDADEIRO ]
+
+//------------------------------------////------------------------------------////-------------------------//
+
+utiliza-se %d // para int ( numero inteiro )
+
+utiliza-se %f // para float ( numero real )
+
+utiliza-se %c // para char com um caracter
+
+utiliza-se %s // para char com vários caracteres
+
+utiliza-se %f // para double (numero real com dupla precisão)
+
+//------------------------------------////------------------------------------////-------------------------//
+
+// a ! significa diferente
+// o = significa igual
+// o < significa menor
+// o > significa maior
+// o <= significa menor ou igual
+// o >= significa maior ou igual
+// o != significa diferente ou igual
