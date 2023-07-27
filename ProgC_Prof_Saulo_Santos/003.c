ffa0ab11c4b2fdf8dda1276562660ecb5b96357e
ThiagoViniciosAnacleto <139421532+ThiagoViniciosAnacleto@users.noreply.github.com>
2023-07-26 23:37:23 -0300

    Aulas com Saulo Santos
    Aqui coloquei exercícios praticados com o Professor Saulo Santos ( Youtube )

---
+++ ProgC_Prof_Saulo_Santos/003.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+    int abacaxi, laranja, total;
+    
+    //Entrada de dados
+
+    laranja = 4;
+    abacaxi = 4;
+
+    //Processamento de dados
+
+    total = laranja + abacaxi;
+
+    //Saida de dados
+
+    printf("%d", total);
+    
+    return 0;
+
+}
---
+++ ProgC_Prof_Saulo_Santos/006p4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main (void){
+
+    int a = 7 , b = 2;
+
+    float c =(float) a / b;
+
+    printf("%f", c);
+
+    return 0;
+}
